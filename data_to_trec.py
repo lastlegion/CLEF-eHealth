799f46daef00f77f5dcfdde87978fd2bb8cca995
Ganesh Iyer <lastlegion@gmail.com>
2014-03-24 23:06:35 +0530

    now converts to trecweb format

--- data_to_trec.py
+++ data_to_trec.py
@@ -1,19 +1,21 @@
-fw = open('wiki_trec_style','w')
-count=0
+fw = open('wiki_trecweb_style','w')
+count =0
 with open('wiki.0842_12.dat','r') as fr:
     state = "doc"
-    for line in fr:        
+    for line in fr:     
+        line = line.strip()
+        line = line.strip("\\")
         wline=""
         if(state == "doc"):
             if(line[0:4] == "#UID"):
-                wline = "<DOC>\n<DOCNO>\n\t"+ str(count)+ "\n</DOCNO>\n"
+                wline = "<DOC>\n<DOCNO>\n\t"+ line[5:len(line)]+ "\n</DOCNO>\n<DOCHDR>\n"
                 state = "hdr"
         elif(state=="hdr"):
                 #print line
                 wline= ""
                 if(line[0:3] == "#CO"):
                     state="content"
-                    #wline+="</DOCHDR>"
+                    wline+="</DOCHDR>"
         elif(state=="content"):
             #print "content state"
             wline=line
